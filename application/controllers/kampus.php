a9924d293c45986e142f0391149475e1169ed358
wp2-dedesofiarahmah <90882903+wp2-dedesofiarahmah@users.noreply.github.com>
2021-10-15 13:04:34 +0700

    Add files via upload

--- application/controllers/kampus.php
+++ application/controllers/kampus.php
@@ -5,6 +5,7 @@ class kampus extends CI_Controller {
 		parent::__construct();
 		$this->load->model('m_data');
 		$this->load->helper('url');
+		$this->load->library('form_validation');
 	}
 	
 	function index() {
@@ -16,19 +17,45 @@ class kampus extends CI_Controller {
 		$this->load->view('input_data');
 	}
 	function tambah_aksi() {
-		$nim = $this->input->post('nim');
-		$nama = $this->input->post('nama');
-		$alamat = $this->input->post('alamat');
-		$pekerjaan = $this->input->post('pekerjaan');
+		$this->form_validation->set_rules('nim','NIM','required|min_length[8] |max_length[8]');
+		$this->form_validation->set_rules('nama','Nama','required|alpha|min_length[5] |max_length[15]');
+		$this->form_validation->set_rules('alamat','Alamat','required|alpha');
+		$this->form_validation->set_rules('pekerjaan','Pekerjaan','required|alpha');
+
+		if($this->form_validation->run() == TRUE)
+		{
+
+			$nim = $this->input->post('nim');
+			$nama = $this->input->post('nama');
+			$alamat = $this->input->post('alamat');
+			$pekerjaan = $this->input->post('pekerjaan');
 		
+			$config['max_size']=2046;
+			$config['allowed_types']="png|jpg|jpeg|gif";
+			$config['remove_spaces']=TRUE;
+			$config['overwrite']=TRUE;
+			$config['upload_path']=FCPATH.'images';
+
+			$this->load->library('upload');
+			$this->upload->initialize($config);
+
+			$this->upload->do_upload('foto');
+			$data_image=$this->upload->data('file_name');
+			$location='images/';
+			$foto=$location.$data_image;
+
 		$data = array (
 			'nim' => $nim,
 			'nama' => $nama,
 			'alamat' => $alamat,
 			'pekerjaan' => $pekerjaan,
+			'foto' => $foto
 			);
 		$this->m_data->input_data($data,'mahasiswa');
 		redirect('kampus/index');
+		}else{
+			$this->load->view('input_data');
+		}
 	}
 	function edit($id) {
 		$where = array('id' => $id);
@@ -42,11 +69,26 @@ class kampus extends CI_Controller {
 		$alamat = $this->input->post('alamat');
 		$pekerjaan = $this->input->post('pekerjaan');
 
+		$config['max_size']=2046;
+		$config['allowed_types']="png|jpg|jpeg|gif";
+		$config['remove_spaces']=TRUE;
+		$config['overwrite']=TRUE;
+		$config['upload_path']=FCPATH.'images';
+
+		$this->load->library('upload');
+		$this->upload->initialize($config);
+
+		$this->upload->do_upload('foto');
+		$data_image=$this->upload->data('file_name');
+		$location='images/';
+		$foto=$location.$data_image;
+
 		$data = array(
 			'nim' => $nim,
 			'nama' => $nama,
 			'alamat' => $alamat,
 			'pekerjaan' => $pekerjaan,
+			'foto' => $foto
 		);
 
 		$where = array(
